aa73190f98c10ddd80e195b8c2b7386f288562f9
Jonathan <Jonathan@berrisch.biz>
2019-11-04 09:01:50 +0100

    Update example code

--- preprocess.R
+++ preprocess.R
@@ -34,10 +34,23 @@ dbWriteTable(con, "stats", stats, overwrite = T)
 dbWriteTable(con, "shift", shift, overwrite = T)
 
 # Use the example:
+
+# library(dplyr)
+# library(tibble)
+# library(RSQLite)
+# library(pool)
 # 
+# # Students Table
 # load(file = "students_example.Rda")
+# # Stats Table => Mainly to sync across sessions
 # stats <- data.frame(shift = c(1,2,3,4), sumstudents = c(0,0,0,0))
+# # shift table => Purely for syncing across sessions
+# shift <- data.frame(shift = c(1))
+# # Create DB directory
 # dir.create("db", showWarnings = F)
+# # Create DB or open connection
 # con <- dbPool(drv = RSQLite::SQLite(), dbname = "db/students_db")
-# dbWriteTable(con, "students", students)
-# dbWriteTable(con, "stats", stats)
+# # Write to DV
+# dbWriteTable(con, "students", students, overwrite = T)
+# dbWriteTable(con, "stats", stats, overwrite = T)
+# dbWriteTable(con, "shift", shift, overwrite = T)
